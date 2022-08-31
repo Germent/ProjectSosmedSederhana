e8e93f7544448dc1889894ca963424cbb8b20eb5
Germent <tibancenter490@gmail.com>
2022-08-31 14:16:32 +0700

    Comment Using Ajax

--- app/Http/Controllers/Post/StoreCommentController.php
+++ app/Http/Controllers/Post/StoreCommentController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Post;
 
 use App\Models\Post;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class StoreCommentController extends Controller
 {
@@ -14,18 +16,32 @@ class StoreCommentController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request, Post $post)
+    public function __invoke(Request $request, $post)
     {
 
-        $data = $request->validate([
-            'body' => ['required', 'min:8']
-        ]);
+        // $data = $request->validate([
+        //     'body' => ['required', 'min:8']
+        // ]);
 
-        $data['user_id'] = $request->user()->id;
+        // $data['user_id'] = $request->user()->id;
 
-        $comment = $post->comments()->create($data);
+        // $comment = $post->comments()->create($data);
+
+        $comms = new Comment;
+        $comms->user_id=Auth::user()->id;
+        $comms->post_id = $post;
+        $comms->body = $request->body;
+        $comms->save();
 
 
-        return redirect()->back()->with('success', 'Comment Added');
+        $dt = [
+            'id' => $comms->id,
+            'name' => Auth::user()->name,
+            'post_id' => $comms->post_id,
+            'body' => $comms->body
+
+        ];
+        return response()->json($dt, 200);
+        // return redirect()->back()->with('success', 'Comment Added');
     }
 }
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,21 +5,11 @@
         </h2>
     </x-slot>
 
-    
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="card w-full bg-base-80 shadow-xl my-3 ">
-                <div class="card-body">
-                  <h2 class="card-title text-gray-500">{{ $post->user->name }} - <span class="">{{ $post->created_at->diffForHumans() }}</span></h2>
-                  <p>{{ $post->body }}</p>
-                  </div>
-                </div>
-            </div>
-
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <form action="{{ route('post.comment.store', $post) }}" method="post" >
+                        <form action="{{ route('post.comment.store', $post) }}" method="post" id="formkomen" >
                             @csrf
                             @error('body')
                             <span class="text-error">{{ $message }}</span>
@@ -32,14 +22,24 @@
         </div>
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="card w-full bg-base-80 shadow-xl my-3 ">
+                <div class="card-body">
+                  <h2 class="card-title text-gray-500">{{ $post->user->name }} - <span class="">{{ $post->created_at->diffForHumans() }}</span></h2>
+                  <p>{{ $post->body }}</p>
+                  </div>
+                </div>
+            </div>
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <h1 class="text-2x1 my-3 text-gray-500">Comments</h1>
+            <form id="form-submit-comment">
         @foreach ($comments as $comment)
                 <div class="card w-full bg-base-80 shadow-xl my-3 ">
                     <div class="card-body">
                       <h2 class="card-title text-gray-500">{{ $comment->user->name }} -
                         <span class="">{{ $comment->created_at->diffForHumans() }}</span>
                        @can('delete', $comment)
-                       <form action="{{ route('post.comment.destroy', [$comment->post, $comment]) }}"
+                       <form id="comment.container" action="{{ route('post.comment.destroy', [$comment->post, $comment]) }}"
                         method="post">
                             @csrf
                             @method('DELETE')
@@ -51,5 +51,41 @@
                       </div>
                     </div>
             @endforeach
+        </form>
         </div>
+
+        <script type="text/javascript" src="{{ asset('/js/jquery-3.6.1.min.js') }}"></script>
+
+        <script type="text/javascript">
+        $("#formkomen").submit(function(e){
+            e.preventDefault();
+            const active = $(this)
+            $.ajax({
+                url: "{{url('/post/'.$post->id.'/comment')}}",
+                type: "POST",
+                data: $("#formkomen").serialize(),
+                success: function(res) {
+                    const component =
+                  `
+                <div class="card w-full bg-base-80 shadow-xl my-3 ">
+                    <div class="card-body">
+                      <h2 class="card-title text-gray-500"> ${res.name}
+                            @csrf
+                            @method('DELETE')
+                            <input type="submit" value="Delete" class=" btn btn-error text-white">
+                            </h2>
+                        <p> ${res.body} </p>
+                    </div>
+                </div>
+                  `
+                    $("#form-submit-comment").append(component);
+                },
+                error: function(result) {
+                    console.log(result);
+                }
+
+            })
+            return false;
+        })
+</script>
 </x-app-layout>
